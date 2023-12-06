11a739d607cc0cea2c6ccf7b4c7752646f407747
Arslan Majeed <arslanmajeed214@gmail.com>
2023-12-06 20:43:35 +0500

    Create ExpenseForm.tsx

---
+++ react-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="description">Description</label>
+        <input id="description" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount">Amount</label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category">Category</label>
+        <select id="category" className="form-select"></select>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
