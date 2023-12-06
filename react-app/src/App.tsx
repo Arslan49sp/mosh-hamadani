2d39ecf77d35006c9b4697c434a12bca86ce0be9
Arslan Majeed <arslanmajeed214@gmail.com>
2023-12-06 19:51:33 +0500

    Expense list component completed.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,9 +7,37 @@ import LikeButton from "./assets/components/LikeButton/LikeButton";
 import Navbar from "./assets/components/NavBar/Navbar";
 import Cart from "./assets/components/Cart/Cart";
 import ExpandableText from "./assets/components/ExpandableText/ExpandableText";
+import Form from "./assets/components/Form/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
   const [alertVisibility, setAlertVisibility] = useState(false);
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "aaa",
+      amount: 20,
+      category: "Electronics",
+    },
+    {
+      id: 2,
+      description: "bbb",
+      amount: 20,
+      category: "Electronics",
+    },
+    {
+      id: 3,
+      description: "ccc",
+      amount: 20,
+      category: "Electronics",
+    },
+    {
+      id: 4,
+      description: "ddd",
+      amount: 20,
+      category: "Electronics",
+    },
+  ]);
   const [products, setProducts] = useState([
     "Lahore",
     "Karanchi",
@@ -23,7 +51,14 @@ function App() {
   };
   return (
     <>
-      <ListGroup
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+      {/* <Form /> */}
+      {/* <ListGroup
         items={items}
         heading="Cities of Pakistan"
         onSelectItem={handleClick}
@@ -71,7 +106,7 @@ function App() {
         minus sequi, architecto soluta quis facere dolorem? Natus alias facere
         earum tempore enim omnis in voluptatibus corporis ut libero adipisci
         aspernatur facilis, consequuntur quaerat.
-      </ExpandableText>
+      </ExpandableText> */}
     </>
   );
 }
