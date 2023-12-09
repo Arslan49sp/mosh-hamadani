f8d884535a260b711f2c3e28ce6a60e4223ba804
Arslan Majeed <arslanmajeed214@gmail.com>
2023-12-09 12:09:19 +0500

    Expense form and module 6 is completed.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -60,7 +60,11 @@ function App() {
   return (
     <>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter onSelect={(category) => setSelectedCategory(category)} />
