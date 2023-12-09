3066dcb4f44ab43aa7fe93f9632b71b64d088802
Arslan Majeed <arslanmajeed214@gmail.com>
2023-12-10 03:09:51 +0500

    Parameterized queries.

---
+++ todo-list/src/react-query/hooks/usePosts.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  }
+
+  const usePosts = (userId: number | undefined) => {
+    const fetchPosts = () =>
+    axios
+    .get('https://jsonplaceholder.typicode.com/posts', {
+        params: {
+            userId
+        }
+    })
+    .then((res) => res.data)
+
+    return useQuery<Post[], Error>({
+        queryKey: ["user", userId,  "posts"],
+        queryFn: fetchPosts,
+        staleTime: 10 * 60 * 1000,
+      });
+  }
+
+  export default usePosts;
