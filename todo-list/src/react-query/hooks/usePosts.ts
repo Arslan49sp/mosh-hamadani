c853381bd06f9f0f9d1de37c3cc450e2821e06cd
Arslan Majeed <arslanmajeed214@gmail.com>
2023-12-18 23:12:10 +0500

    Update usePosts.ts

--- todo-list/src/react-query/hooks/usePosts.ts
+++ todo-list/src/react-query/hooks/usePosts.ts
@@ -10,7 +10,7 @@ interface Post {
 const usePosts = (userId: number | undefined) => {
   const fetchPosts = () =>
     axios
-      .get("https://xsssjsonplaceholder.typicode.com/posts", {
+      .get("https://jsonplaceholder.typicode.com/posts", {
         params: {
           userId,
         },
