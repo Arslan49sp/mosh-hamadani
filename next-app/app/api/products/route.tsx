c370746aae8228f0e713a61bc18a13224fb8f60f
Arslan Majeed <arslanmajeed214@gmail.com>
2025-02-24 22:51:45 +0500

    validate with zod and complete the exercise.

--- next-app/app/api/products/route.tsx
+++ next-app/app/api/products/route.tsx
@@ -3,8 +3,8 @@ import schema from "./schema";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
-    { id: 1, name: "Arslan" },
-    { id: 2, name: "majeed" },
+    { id: 1, name: "Milk", price: 2.5 },
+    { id: 2, name: "Bread", price: 3.5 },
   ]);
 }
 
@@ -18,5 +18,8 @@ export async function POST(request: NextRequest) {
       { status: 400 }
     );
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  return NextResponse.json(
+    { id: 1, name: body.name, price: body.price },
+    { status: 201 }
+  );
 }
