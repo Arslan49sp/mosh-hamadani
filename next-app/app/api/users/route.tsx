297500ca0e46b0fe167cd2da807212dda148b9f0
Arslan Majeed <arslanmajeed214@gmail.com>
2025-02-24 21:59:31 +0500

    Create an object.

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -6,3 +6,15 @@ export function GET(request: NextRequest) {
     { id: 2, name: "majeed" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json(
+      { error: "you need to provide the name." },
+      { status: 400 }
+    );
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
