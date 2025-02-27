1dd561d378a8bcf9074ac2f3992300afaaaaf1d0
Arslan Majeed <arslanmajeed214@gmail.com>
2025-02-27 17:03:29 +0500

    accessing session on the server

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,6 +1,11 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { authOption } from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+  const session = await getServerSession(authOption);
   return (
     <main>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <button className="btn btn-primary ">Button</button>
     </main>
   );
